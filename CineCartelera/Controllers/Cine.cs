8e294b412fcddeb32707b51c0437a5ffdc7a42f9
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2021-01-18 13:35:23 -0800

    x

---
+++ CineCartelera/Controllers/Cine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CineCartelera.Controllers
+{
+    public class Cine
+    {
+        public int Sala { get; set; }
+        public DateTime Horario { get; set; }
+        public string Clasificacion { get; set; }
+    }
+}
---
+++ CineCartelera/Controllers/CineBDConnetions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CineCartelera.Controllers
+{
+    public class CineBDConnetions:DbContext
+    {
+        public CineBDConnetions() : base("cinedb")
+        {
+
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Cine> Cine { get; set; }
+    }
+}
